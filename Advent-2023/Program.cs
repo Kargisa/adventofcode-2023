bfd18a9178f0de5bb2bd0ba6c290fd4be35dd65c
Kargisa <luca.wolf@outlook.com>
2023-12-03 18:56:36 +0100

    day three

--- Advent-2023/Program.cs
+++ Advent-2023/Program.cs
@@ -1,4 +1,16 @@
 ﻿using Advent_2023;
 using System;
 
-Console.WriteLine(Solutions.Day2_2("../../../day2"));
+//Day 1
+//Console.WriteLine(Solutions.Day1_1("../../../day1"));
+//Console.WriteLine(Solutions.Day1_2("../../../day1"));
+
+//Day 2
+//Console.WriteLine(Solutions.Day2_1("../../../day2"));
+//Console.WriteLine(Solutions.Day2_2("../../../day2"));
+
+//Day 3
+//Console.WriteLine(Solutions.Day3_1("../../../day3"));
+Console.WriteLine(Solutions.Day3_2("../../../day3"));
+
+//Day 4
--- Advent-2023/Solutions.cs
+++ Advent-2023/Solutions.cs
@@ -71,6 +71,38 @@ namespace Advent_2023
             return output;
         }
 
+        private static string WordToNumber(string word)
+        {
+            return word switch
+            {
+                "one" => "1",
+                "two" => "2",
+                "three" => "3",
+                "four" => "4",
+                "five" => "5",
+                "six" => "6",
+                "seven" => "7",
+                "eight" => "8",
+                "nine" => "9",
+                _ => word,
+            };
+        }
+        public struct IndexValuePair
+        {
+            public int MinIndex { get; set; }
+            public string MinValue { get; set; }
+            public int MaxIndex { get; set; }
+            public string MaxValue { get; set; }
+
+            public IndexValuePair()
+            {
+                MinIndex = int.MaxValue;
+                MinValue = "";
+                MaxIndex = int.MinValue;
+                MaxValue = "";
+            }
+        }
+
         // Day 2
         public static int Day2_1(string path)
         {
@@ -113,7 +145,6 @@ namespace Advent_2023
             //1734
             return output;
         }
-
         public static int Day2_2(string path)
         {
             string[] values = File.ReadAllLines(path);
@@ -146,38 +177,185 @@ namespace Advent_2023
             return output;
         }
 
-        private static string WordToNumber(string word)
+        //Day 3
+        public static int Day3_1(string path)
         {
-            return word switch
+            string[] values = File.ReadAllLines(path);
+            int output = 0;
+
+            for (int i = 0; i < values.Length; i++)
             {
-                "one" => "1",
-                "two" => "2",
-                "three" => "3",
-                "four" => "4",
-                "five" => "5",
-                "six" => "6",
-                "seven" => "7",
-                "eight" => "8",
-                "nine" => "9",
-                _ => word,
-            };
+                string line = values[i];
+                NumberInEngine engineNum = new();
+                for (int j = 0; j < line.Length; j++)
+                {
+                    if (int.TryParse(line[j].ToString(), out int num))
+                    {
+                        if (string.IsNullOrEmpty(engineNum.NumberString))
+                        {
+                            engineNum = new NumberInEngine(num.ToString(), j, i, 1);
+                            continue;
+                        }
+                        engineNum.NumberString += num;
+                        engineNum.Length++;
+                        if (j != line.Length - 1)
+                            continue;
+                    }
+                    if (string.IsNullOrEmpty(engineNum.NumberString))
+                        continue;
+                    if (AdjecentToSymbol(engineNum, values))
+                        output += int.Parse(engineNum.NumberString);
+                    engineNum = new();
+                }
+            }
+            //560670
+            return output;
         }
-    }
-
-    public struct IndexValuePair
-    {
-        public int MinIndex { get; set; }
-        public string MinValue { get; set; }
-        public int MaxIndex { get; set; }
-        public string MaxValue { get; set; }
-
-        public IndexValuePair()
+        //Can be shortened
+        private static bool AdjecentToSymbol(NumberInEngine engineNum, string[] lines)
         {
-            MinIndex = int.MaxValue;
-            MinValue = "";
-            MaxIndex = int.MinValue;
-            MaxValue = "";
+            string line = lines[engineNum.LineIndex];
+            if (engineNum.LineIndex != 0)
+            {
+                for (int k = engineNum.StartIndex - 1; k < engineNum.StartIndex + engineNum.Length + 1; k++)
+                {
+                    if (k < 0 || k >= line.Length)
+                        continue;
+                    if (lines[engineNum.LineIndex - 1][k] != '.')
+                        return true;
+                }
+            }
+            if (engineNum.LineIndex != lines.Length - 1)
+            {
+                for (int k = engineNum.StartIndex - 1; k < engineNum.StartIndex + engineNum.Length + 1; k++)
+                {
+                    if (k < 0 || k >= line.Length)
+                        continue;
+                    if (lines[engineNum.LineIndex + 1][k] != '.')
+                        return true;
+                }
+                if (engineNum.StartIndex != 0)
+                {
+                    if (lines[engineNum.LineIndex][engineNum.StartIndex - 1] != '.')
+                        return true;
+                }
+                if (engineNum.StartIndex + engineNum.Length < line.Length)
+                {
+                    if (lines[engineNum.LineIndex][engineNum.StartIndex + engineNum.Length] != '.')
+                        return true;
+                }
+            }
+            return false;
         }
+        public static int Day3_2(string path)
+        {
+            string[] values = File.ReadAllLines(path);
+            int output = 0;
+
+            Dictionary<string, int> numberGears = new();
+
+            for (int i = 0; i < values.Length; i++)
+            {
+                string line = values[i];
+                NumberInEngine engineNum = new();
+                for (int j = 0; j < line.Length; j++)
+                {
+                    if (int.TryParse(line[j].ToString(), out int num))
+                    {
+                        if (string.IsNullOrEmpty(engineNum.NumberString))
+                        {
+                            engineNum = new NumberInEngine(num.ToString(), j, i, 1);
+                            continue;
+                        }
+                        engineNum.NumberString += num;
+                        engineNum.Length++;
+                        if (j != line.Length - 1)
+                            continue;
+                    }
+                    if (string.IsNullOrEmpty(engineNum.NumberString))
+                        continue;
+
+                    output += AdjecentToGear(engineNum, values, numberGears);
+                    engineNum = new();
+                }
+            }
+            //91622824
+            return output;
+        }
+        //Can be shortened
+        private static int AdjecentToGear(NumberInEngine engineNum, string[] lines, Dictionary<string, int> numberGears)
+        {
+            string line = lines[engineNum.LineIndex];
+            if (engineNum.LineIndex != 0)
+            {
+                for (int k = engineNum.StartIndex - 1; k < engineNum.StartIndex + engineNum.Length + 1; k++)
+                {
+                    if (k < 0 || k >= line.Length)
+                        continue;
+                    if (lines[engineNum.LineIndex - 1][k] == '*')
+                    {
+                        string id = $"{engineNum.LineIndex - 1}{k}";
+                        int number = int.Parse(engineNum.NumberString);
+                        if (!numberGears.TryAdd(id, number))
+                            return int.Parse(engineNum.NumberString) * numberGears[id];
+                    }
+                }
+            }
+            if (engineNum.LineIndex != lines.Length - 1)
+            {
+                for (int k = engineNum.StartIndex - 1; k < engineNum.StartIndex + engineNum.Length + 1; k++)
+                {
+                    if (k < 0 || k >= line.Length)
+                        continue;
+                    if (lines[engineNum.LineIndex + 1][k] == '*')
+                    {
+                        string id = $"{engineNum.LineIndex + 1}{k}";
+                        int number = int.Parse(engineNum.NumberString);
+                        if (!numberGears.TryAdd(id, number))
+                            return int.Parse(engineNum.NumberString) * numberGears[id];
+                    }
+                }
+            }
+            if (engineNum.StartIndex != 0)
+            {
+                if (lines[engineNum.LineIndex][engineNum.StartIndex - 1] == '*')
+                {
+                    string id = $"{engineNum.LineIndex}{engineNum.StartIndex - 1}";
+                    int number = int.Parse(engineNum.NumberString);
+                    if (!numberGears.TryAdd(id, number))
+                        return int.Parse(engineNum.NumberString) * numberGears[id];
+                }
+            }
+            if (engineNum.StartIndex + engineNum.Length < line.Length)
+            {
+                if (lines[engineNum.LineIndex][engineNum.StartIndex + engineNum.Length] == '*')
+                {
+                    string id = $"{engineNum.LineIndex}{engineNum.StartIndex + engineNum.Length}";
+                    int number = int.Parse(engineNum.NumberString);
+                    if (!numberGears.TryAdd(id, number))
+                        return int.Parse(engineNum.NumberString) * numberGears[id];
+                }
+            }
+            return 0;
+        }
+        private struct NumberInEngine
+        {
+            public string NumberString { get; set; }
+            public int StartIndex { get; set; }
+            public int LineIndex { get; set; }
+            public int Length { get; set; }
+
+            public NumberInEngine(string numberString, int startIndex, int lineIndex, int length)
+            {
+                NumberString = numberString;
+                StartIndex = startIndex;
+                LineIndex = lineIndex;
+                Length = length;
+            }
+        }
+
+        //Day 4
     }
+
 
 }
